21bb7d13905fb3f82b8a3630ab13b3ee2fe9981c
NellowTCS <neeljaiswal23@gmail.com>
2025-06-03 01:48:38 +0000

    fixes

---
+++ Build/vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+  ],
+  build: {
+    rollupOptions: {
+      input: '/src/main.ts',
+    },
+  },
+});
