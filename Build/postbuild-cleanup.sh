ec2437a9cf65791e795d13bbac0a20af7d439b4f
None <neeljaiswal23@gmail.com>
2025-06-10 01:33:15 +0000

    test

--- Build/postbuild-cleanup.sh
+++ Build/postbuild-cleanup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Remove extra src/index.html from dist after Vite build
+# Remove extra src/index.html from dist after Vite build 
 cp -f ./dist/src/index.html ./dist/index.html
 rm -f ./dist/src/index.html
 rmdir --ignore-fail-on-non-empty ./dist/src
